61a5dd4f088202f6d53490608dd6481400b8eb93
thomas-lando-18 <thomaslando6@gmail.com>
2024-10-06 15:11:59 +1030

    added wing selection form

--- src/app/wing-shape-selection/wing-shape-selection.component.ts
+++ src/app/wing-shape-selection/wing-shape-selection.component.ts
@@ -1,23 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { WingShape } from './wing-shape';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-wing-shape-selection',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './wing-shape-selection.component.html',
   styleUrl: './wing-shape-selection.component.css'
 })
 export class WingShapeSelectionComponent {
   validCamber: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   validThickness : Array<number> = Array.from(Array(99).keys());
+  wingShape: WingShape
+  wingForm: FormGroup;
 
-  model = new WingShape(2, 4, 12)
+  constructor(private fb: FormBuilder) {
+    this.wingForm = this.fb.group(
+      {
+        number1: [null, [Validators.required, Validators.pattern('^[0-9]+$')]],
+        number2: [null, [Validators.required, Validators.pattern('^[0-9]+$')]],
+        number3: [null, [Validators.required, Validators.min(0), Validators.max(99)]],
+      }
+    )
 
-  submitted: boolean = false;
+    this.wingShape = new WingShape(
+      this.wingForm.value.number1,
+      this.wingForm.value.number2,
+      this.wingForm.value.number3 
+    )
+  }
 
   onSubmit() {
-    this.submitted = true;
+    // this.submitted = true;
     console.log("The button has been pressed")
+    this.wingShape = new WingShape(
+      this.wingForm.value.number1,
+      this.wingForm.value.number2,
+      this.wingForm.value.number3 
+    )
+
+    console.log(this.wingShape)
   }
+
+  
 }
